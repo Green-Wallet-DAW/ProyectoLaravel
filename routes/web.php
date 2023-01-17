380f88212ab38c11bf7a242817dace9272ec6451
Borja Clase <bvalls@iesfuentesanluis.org>
2023-01-17 14:43:57 +0100

    Cambios cambios cambios

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/prueba','prueba');
+Route::view('/usuarios','usuarios');
