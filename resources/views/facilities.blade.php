2fedacedc0cfc239e74ed9a83a28c1e8c8fcf589
Borja Clase <bvalls@iesfuentesanluis.org>
2023-01-20 09:56:51 +0100

    Nueva rama

--- resources/views/facilities.blade.php
+++ resources/views/facilities.blade.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/plantilla.css">
-    <title>Document</title> 
+    <link rel="stylesheet" href="/css/facilities.css">
+
+    <title>Document</title>
 </head>
 <body>
     <nav id="nav" class="navbar navbar-expand-xl navbar-light d-none d-xl-block">
@@ -50,46 +52,53 @@
       <div id="cabz"><a href="#">Green Wallets</a></div>
     </div>
     <div class="row">
-      <div class="col-3">
-        <div class="aside flex-shrink-0" >
-          <a href="/" class="d-flex align-items-center justify-content-center py-2 mb-3 text-decoration-none border-bottom">
-            <span class="fs-1 text-light  fw-semibold">DASHBOARD</span>
-          </a>
-          <ul class="list-unstyled ps-0">
-            <li class="mb-1">
-              <button class="btn btn-toggle align-items-center rounded collapsed fs-3 text-light" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                QUERIES
-              </button>
-              <div class="subelement collapse show" id="home-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                  <li><a href="#" class="link-light rounded fs-4">Users</a></li>
-                  <li><a href="#" class="link-light rounded fs-4">Communities</a></li>
-                  <li><a href="#" class="link-light rounded fs-4">Facilities</a></li>
-                  <li><a href="#" class="link-light rounded fs-4">Machines</a></li>
-                </ul>
-              </div>
-            </li>
-            <li class="mb-1">
-              <button class="btn btn-toggle align-items-center rounded collapsed fs-3 text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                TOKEN STORE
-              </button>
-              <div class="subelement collapse" id="orders-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                  <li><a href="#" class="link-light rounded fs-4">User Services</a></li>
-                  <li><a href="#" class="link-light rounded fs-4">Community Services</a></li>
-                </ul>
-              </div>
-            </li>
-            <li class="border-top my-3 "></li>
-        
-          </ul>
+        <div class="col-3">
+
+            <div class="aside flex-shrink-0" >
+            <a href="/" class="d-flex align-items-center justify-content-center py-2 mb-3 text-decoration-none border-bottom">
+                <span class="fs-1 text-light  fw-semibold">DASHBOARD</span>
+            </a>
+            <ul class="list-unstyled ps-0">
+                <li class="mb-1">
+                <button class="btn btn-toggle align-items-center rounded collapsed fs-3 text-light" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+                    QUERIES
+                </button>
+                <div class="subelement collapse show" id="home-collapse">
+                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-light rounded fs-4">Users</a></li>
+                    <li><a href="#" class="link-light rounded fs-4">Communities</a></li>
+                    <li><a href="#" class="link-light rounded fs-4">Facilities</a></li>
+                    <li><a href="#" class="link-light rounded fs-4">Machines</a></li>
+                    </ul>
+                </div>
+                </li>
+                <li class="mb-1">
+                <button class="btn btn-toggle align-items-center rounded collapsed fs-3 text-light" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+                    TOKEN STORE
+                </button>
+                <div class="subelement collapse" id="orders-collapse">
+                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-light rounded fs-4">User Services</a></li>
+                    <li><a href="#" class="link-light rounded fs-4">Community Services</a></li>
+                    </ul>
+                </div>
+                </li>
+                <li class="border-top my-3 "></li>
+
+            </ul>
+            </div>
+
         </div>
-      </div>
-      <div class="col-9">
-        @yield('contenidoPagina')
-      </div>
+           {{-- Facilities --}}
+
+        <div class="col-8">
+            
+        </div>
     </div>
-    {{-- <h1>USUARIOS</h1> --}}
+
+
+
+
     <div class="footer bg-dark-subtle">
         <p class="copyright">Copyright © 2022 Green Wallets - All Rights Reserved.</p>
         <a class="privacy" href="https://wallets.green/privacy-policy">PRIVACY POLICY</a>
