4ce00cd0fa9bd6058c41165e2e2797679115caeb
BorjaValls <91543102+BorjaValls@users.noreply.github.com>
2023-01-25 08:33:42 +0100

    Add files via upload
    Nuevas migraciones

---
+++ database/migrations/2023_01_17_111354_create_usuarios_servicios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios_servicios', function (Blueprint $table) {
+            $table->integer('id_service')->unsigned();
+            $table->integer('id_user')->unsigned();
+            $table->primary(array('id_service','id_user'));
+            $table->foreign('id_service')->references('id')->on('servicios');
+            $table->foreign('id_user')->references('id')->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios_servicios');
+    }
+};
