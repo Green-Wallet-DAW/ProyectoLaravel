3eba4b7e4f646bba51aaa76b986c0c5f047031bc
Joaquin Lucero <jlucero@iesfuentesanluis.org>
2023-01-30 10:30:53 +0100

    Formularios bonitos

--- resources/views/editServices.blade.php
+++ resources/views/editServices.blade.php
@@ -1,4 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/serviceList.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
 @extends('plantilla')
+
+
 @section('contenidoPagina')
 
 @if ($errors->any())
@@ -12,29 +30,32 @@
         </div>
     @endif
 
-<form method="get" action="{{ route('updateService', $item) }}">
-        <div class="form-group"> 
+<div id="caja" class="caja">
+    <form method="get" action="{{ route('updateService', $item) }}">
+        <div class="form-group formularioCaja"> 
 
               @csrf
               
               <label for="name">Name:</label>
               <input class="form-control" type="text"  name="name" value="{{ $item->name }}"/>
           </div>
-          <div class="form-group">
+          <div class="form-group formularioCaja">
             <label for="description">Description</label>
             <input  class="form-control" type="text"  name="description" value="{{ $item->description }}"/>
         </div>
-        <div class="form-group">
+        <div class="form-group formularioCaja">
             <label for="link">Link</label>
             <input class="form-control" type="text"  name="link" value="{{ $item->link }}"/>
         </div>
-        <div class="form-group">
+        <div class="form-group formularioCaja">
             <label for="precio">Price</label>
-            <input class="form-control" type="text"  name="precio" value="{{ $item->precio }}"/>
+            <input class="form-control" type="number"  name="precio" value="{{ $item->precio }}"/>
         </div>
-          <button class="btn btn-success" type="submit" >Update</button>
+          <button class="btn btn-success botonFormulario" type="submit" >Update</button>
       </form>
-  </div>
 </div>
+
+
+
 
 @endsection
