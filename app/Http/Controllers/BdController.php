c45ee24feee3ec6dffe0d1407b5d45fafe49ee73
croaua <croaua@iesfuentesanluis.org>
2023-01-31 14:44:20 +0100

    proyecto1

--- app/Http/Controllers/BdController.php
+++ app/Http/Controllers/BdController.php
@@ -12,17 +12,30 @@ use App\Models\Servicio;
 class BdController extends Controller
 {
     public function unirseacomunidad(){
-       $sql='SELECT comunidades.name, sum(maquinas.energy_produced)as energy_produced,count(usuarios_comunidades.id_user) as members FROM `usuarios_comunidades`
-       inner join comunidades on usuarios_comunidades.id_comunity=comunidades.id
-       inner join usuarios on usuarios.id=usuarios_comunidades.id_user
-         inner join instalaciones on instalaciones.id_user=usuarios.id
-        inner join maquinas on maquinas.id_instalation=instalaciones.id
-       group by comunidades.name;';
-       $comunidades=DB::select($sql);
-    return $comunidades;
+   //     $sql='SELECT comunidades.id, comunidades.name, comunidades.description as descripcion, sum(maquinas.energy_produced)as energy_produced,count(usuarios_comunidades.id_user) as members FROM `usuarios_comunidades`
+   //     inner join comunidades on usuarios_comunidades.id_comunity=comunidades.id
+   //     inner join usuarios on usuarios.id=usuarios_comunidades.id_user
+   //       inner join instalaciones on instalaciones.id_user=usuarios.id
+   //      inner join maquinas on maquinas.id_instalation=instalaciones.id
+   //     group by comunidades.id;';
+   //     $comunidades=DB::select($sql);
+   //  return $comunidades;
+
+      // $unirse1= Comunidad::select('id','name','description')
+      // ->groupBy('id')
+      // ->get();
+      $unirse2= Comunidad::join('usuarios_comunidades','comunidades.id','=','usuarios_comunidades.id_comunity')
+      ->join('usuarios','usuarios.id','=','usuarios_comunidades.id_user')
+      ->join('instalaciones','usuarios.id','=','instalaciones.id_user')
+      ->join('maquinas','instalaciones.id','=','maquinas.id_instalation')
+      ->select(DB::raw('comunidades.id,comunidades.name,comunidades.description,sum(maquinas.energy_produced) as energy_produced, count(usuarios_comunidades.id_user) as members'))
+      ->groupBy('comunidades.id')
+      ->get();
+      //https://www.w3schools.com/sql/func_mysql_if.asp
+      return $unirse2;
     }
     public function miscomunidades(){
-        $sql='SELECT comunidades.name, comunidades.token, comunidades.description, sum(maquinas.carbono_ahorrado)as carbono_ahorrado, sum(maquinas.energy_produced)as energy_produced FROM `usuarios_comunidades`
+        $sql='SELECT comunidades.id, comunidades.name, comunidades.token, comunidades.description, sum(maquinas.carbono_ahorrado)as carbono_ahorrado, sum(maquinas.energy_produced)as energy_produced FROM `usuarios_comunidades`
         inner join comunidades on usuarios_comunidades.id_comunity=comunidades.id
         inner join usuarios on usuarios.id=usuarios_comunidades.id_user
         inner join instalaciones on instalaciones.id_user=usuarios.id
@@ -30,8 +43,25 @@ class BdController extends Controller
         group by comunidades.id';
         $comunidades=DB::select($sql);
      return $comunidades;
-
+    }
+    public function misusuarios(){
+      
+   //    $sql="SELECT usuarios.name as members, sum(maquinas.energy_produced)as energy_produced, sum(maquinas.carbono_ahorrado)as carbono_ahorrado FROM `usuarios` inner join instalaciones on instalaciones.id_user=usuarios.id inner join maquinas on maquinas.id_instalation=instalaciones.id inner join usuarios_comunidades on usuarios_comunidades.id_user=usuarios.id where usuarios_comunidades.id_comunity=$num group by usuarios.name";
+   //    $comunidades=DB::select($sql);
+   //    return $comunidades;
+   // //   SELECT usuarios.name as members, sum(maquinas.energy_produced)as energy_produced, sum(maquinas.carbono_ahorrado)as carbono_ahorrado FROM `usuarios` inner join instalaciones on instalaciones.id_user=usuarios.id inner join maquinas on maquinas.id_instalation=instalaciones.id group by maquinas.id_instalation;
      }
+     public function totalPro(){
+      
+         $sql="SELECT name as member, sum(maquinas.energy_produced)as Total_Production, sum(maquinas.carbono_ahorrado)as Total_carbon_saved FROM `usuarios`
+         inner join instalaciones on usuarios.id=instalaciones.id_user
+         inner join maquinas on instalaciones.id=maquinas.id_instalation
+         group by member
+         order by Total_production desc";
+         $comunidades=DB::select($sql);
+         return $comunidades;
+      
+        }
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -60,3 +60,5 @@ Route::get('/maquina/buscar/{id}', [MaquinaController::class, 'show']);
 
 Route::get('/unirseacomunidad', [BdController::class, 'unirseacomunidad']);
 Route::get('/miscomunidades', [BdController::class, 'miscomunidades']);
+Route::get('/misusuarios', [BdController::class, 'misusuarios']);
+Route::get('/globalcoms', [BdController::class, 'totalPro']);
