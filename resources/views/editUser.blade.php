fad1ec4648acc36c3a470577bdb6921d9befcd83
BorjaValls <bvalls@iesfuentesanluis.org>
2023-04-05 16:46:10 +0100

    5 de abril

--- resources/views/editUser.blade.php
+++ resources/views/editUser.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('/css/users.css')}}">
-    <link rel="stylesheet" href="{{asset('/css/editUser.css')}}">
     <link rel="stylesheet" href="{{asset('/css/plantillaUser.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/users.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/pt.css')}}">
     <title>User data edition</title>
 </head>
 <body>
--- resources/views/telegram.blade.php
+++ resources/views/telegram.blade.php
@@ -8,8 +8,9 @@
     <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('/css/users.css')}}">
     <link rel="stylesheet" href="{{asset('/css/plantillaUser.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/users.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/pt.css')}}">
     <title>Telegram</title>
 </head>
 <body>
@@ -23,20 +24,30 @@
         <div id="cUser">
           <a href="{{route('createTelegram')}}"><button type="button" class="btn btn-success fs-5">New message</button></a>
         </div>
-                @forelse ($telegram as $item)
-                  <div>
-                    <p>{{$item->name}}</p>
-                    <p>{{$item->content}}</p>
-                    <p>{{$item->created_at}}</p>
-                    <p>{{$item->updated_at}}</p>
-                    <p>
-                      <a href="{{route('send', $item->id)}}" class="btn btn-primary" data-bs-toggle="tooltip" title="Send message">Send message</a>
-                    </p>
-                  </div>
+          <div class="row">
+            @forelse ($telegram as $item)
+                  {{-- Diseñar nueva caja para poder hacer filas de 2 y que quepan en la pantalla --}}
+                  {{-- <div class="col-6"> --}}
+                    <div class="userBox">
+                      <div class="uCon fs-1 pt-4">
+                          <strong>{{$item->name}}</strong>
+                      </div>
+                      <div class="uCon">
+                          <strong>Name: </strong>{{$item->name}}
+                      </div>
+                      <div class="uCon">
+                          <strong>Content: </strong><br>
+                          <textarea name="content" id="" cols="65" rows="5" disabled>{{$item->content}}</textarea>
+                      </div>
+                      <div class="uCon">
+                        <a href="{{route('send', $item->id)}}" class="btn btn-primary" data-bs-toggle="tooltip" title="Send message">Send message</a>
+                      </div>
+                    </div>
+                  {{-- </div> --}}
                 @empty
                     <div>TELEGRAMS DO NOT EXIST</div>
                 @endforelse
-
+          </div>
       </div>
     @endsection
   @endguest
